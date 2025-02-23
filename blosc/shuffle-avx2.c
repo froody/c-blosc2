9c2942e981059c51a8cec4169a49f4e9c43c8713
Tom Birch <tom@neara.com>
2025-02-23 22:36:06 +1100

    48

--- blosc/shuffle-avx2.c
+++ blosc/shuffle-avx2.c
@@ -293,9 +293,11 @@ shuffle12_avx2(uint8_t* const dest, const uint8_t* const src,
     //
     printymm32("xH",ymm0[0]);
     ymm0[0] = _mm256_permute4x64_epi64(ymm0[0], 0b00111001);
+    ymm0[1] = _mm256_permute4x64_epi64(ymm0[1], 0b00111001);
 
     printymm32("H",ymm0[0]);
     ymm1[4] =   _mm256_unpacklo_epi8(ymm0[4], ymm0[0]);
+    ymm1[5] =   _mm256_unpacklo_epi8(ymm0[5], ymm0[1]);
 
 
     printymm32("F2",ymm0[1]);
@@ -311,6 +313,7 @@ shuffle12_avx2(uint8_t* const dest, const uint8_t* const src,
     _mm256_storeu_si256((__m256i*)(dest_for_jth_element + (2 * total_elements)), ymm1[2]);
     _mm256_storeu_si256((__m256i*)(dest_for_jth_element + (3 * total_elements)), ymm1[3]);
     _mm256_storeu_si256((__m256i*)(dest_for_jth_element + (4 * total_elements)), ymm1[4]);
+    _mm256_storeu_si256((__m256i*)(dest_for_jth_element + (5 * total_elements)), ymm1[5]);
     //for (k = 0; k < 12; k++) {
     //  printf("k:%d, %d, %zu, %d\n",k, k * total_elements, offset, j);
     //  _mm256_storeu_si256((__m256i*)(dest_for_jth_element + (k * total_elements)), ymm0[k]);
